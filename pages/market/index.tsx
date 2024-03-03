47926b5d55e62bfdb7009ac5d330e54419da4aa9
hwang <1201q@naver.com>
2024-03-03 15:08:19 +0900

    [fix] /market에서 isLogin이 set되지않도록

--- pages/market/index.tsx
+++ pages/market/index.tsx
@@ -14,7 +14,6 @@ import { useList } from "@/utils/hooks/useList";
 
 import { useUpbitAll, useUpbitSingle } from "@/utils/ws/control";
 import { isLoginAtom } from "@/context/user";
-import { useAtom } from "jotai";
 
 export default function Home({ pathname, isLogin }: ServerSideProps) {
   useHydrateAtoms([[pathnameAtom, pathname]] as ServerSideInitialValues, {
@@ -27,12 +26,10 @@ export default function Home({ pathname, isLogin }: ServerSideProps) {
   const { coinList } = useList();
   const { all } = useUpbitAll(coinList.code);
   const { single } = useUpbitSingle("");
-  const [, setIsLogin] = useAtom(isLoginAtom);
 
   useEffect(() => {
     all.open();
     single.close();
-    isLogin !== undefined && setIsLogin(isLogin);
   }, []);
 
   console.log(`마켓 :${isLogin}`);
@@ -52,7 +49,6 @@ export const getServerSideProps: GetServerSideProps = async (
   const coinList = await fetcher("/api/markets");
 
   const { isLogin, uid } = await getAuth(cookies.token);
-  console.log("서버사이드");
 
   return {
     props: {
