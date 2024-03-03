3eb2f655c40316396db81f0f4b3112ca9803ef3c
hwang <1201q@naver.com>
2024-03-03 15:16:58 +0900

    auth 페이지에서도 useHydrateAtoms를 사용

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -6,14 +6,19 @@ import {
 } from "@/types/types";
 import { useHydrateAtoms } from "jotai/utils";
 import { pathnameAtom } from "@/context/atoms";
+import { isLoginAtom } from "@/context/user";
 import PageRender from "@/components/shared/PageRender";
 import MainPage from "@/components/auth/MainPage";
 
 import nookies from "nookies";
 import getAuth from "@/utils/common/getAuth";
 
-export default function Home({ pathname }: ServerSideProps) {
+export default function Home({ pathname, isLogin }: ServerSideProps) {
   useHydrateAtoms([[pathnameAtom, pathname]] as ServerSideInitialValues, {
+    dangerouslyForceHydrate: true,
+  });
+
+  useHydrateAtoms([[isLoginAtom, isLogin]] as ServerSideInitialValues, {
     dangerouslyForceHydrate: true,
   });
 
