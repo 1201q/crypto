a095b9a87a0e3e86e4757dbd575f57fa5d5b38c2
hwang <1201q@naver.com>
2024-03-07 10:05:29 +0900

    [fix] 배포 오류 수정

--- pages/test.tsx
+++
@@ -1,6 +0,0 @@
-import InitSetting from "@/components/auth/InitSetting";
-import PageRender from "@/components/shared/PageRender";
-
-export default function Home() {
-  return <PageRender Render={InitSetting} />;
-}
